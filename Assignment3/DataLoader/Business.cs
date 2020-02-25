fa6fca8fd5adccd065f11460bd8bf269d36498d5
dotNet504 <60148034+dotNet504@users.noreply.github.com>
2020-02-25 16:11:19 -0600

    Swathi: Added comments

--- Assignment3/DataLoader/Business.cs
+++ Assignment3/DataLoader/Business.cs
@@ -1,4 +1,16 @@
-﻿using System;
+/*********************************************************************************************************
+ *                                                                                                       *
+ *  CSCI:504-MSTR PROGRAMMING PRINCIPLES IN .NET	      Assignment 3					 Spring 2020     *                                          
+ *																										 *
+ *  Programmer's: Swathi Reddy Konatham (Z1864290),
+ *                Abdulsalam Olaoye (Z1836477),
+ *                Xuezhi Cang (Z1747635)                                                                 *  	                           
+ *																										 *
+ *  Class Name: Business
+ *  Purpose   : Windows application that displays the Business details of Dekalb & Sycamore Communities. *
+ *********************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +20,16 @@ namespace DataLoader
 {
     public class Business : Property
     {
+        //variables declaration
         private String name;
         private readonly BusinessType type; //?????????
         private readonly string yearEstablished;
         private uint activeRecruitment;
+        
+        //enumerator declaration
         public enum BusinessType { Grocery, Bank, Repair, FastFood, DepartmentStore };
 
+        //Constructor with arguments
         public Business(String[] args) : base(args)
         {
             name = args[9];
@@ -23,6 +39,7 @@ namespace DataLoader
 
         }
 
+        //getter and setter for Name
         public String Name
         {
             set { name = value; }
@@ -34,11 +51,13 @@ namespace DataLoader
         //    get { return type; }
         //}
 
+        //Getter for YearEstablished readonly property
         public String YearEstablished
         {
             get { return yearEstablished; }
         }
 
+        //getter and setter for ActiveRecruitment
         public uint ActiveRecruitment
         {
             set { activeRecruitment = value; }
