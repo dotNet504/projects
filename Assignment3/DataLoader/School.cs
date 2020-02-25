c89da59ac26e37c7bb46daf62be6ab2fea89f479
dotNet504 <60148034+dotNet504@users.noreply.github.com>
2020-02-25 16:08:05 -0600

    Swathi: Added Comments

--- Assignment3/DataLoader/School.cs
+++ Assignment3/DataLoader/School.cs
@@ -1,4 +1,15 @@
-﻿using System;
+/*********************************************************************************************************
+ *                                                                                                       *
+ *  CSCI:504-MSTR PROGRAMMING PRINCIPLES IN .NET	      Assignment 3					 Spring 2020     *                                          
+ *																										 *
+ *  Programmer's: Swathi Reddy Konatham (Z1864290),
+ *                Abdulsalam Olaoye (Z1836477),
+ *                Xuezhi Cang (Z1747635)                                                                 *  	                           
+ *																										 *
+ *  Class Name: School
+ *  Purpose   : Windows application that displays the school details of Dekalb & Sycamore Communities.   *
+ *********************************************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +19,37 @@ namespace DataLoader
 {
     public class School : Property
     {
+        //variables declaration
         private string name;
         private readonly SchoolType type;
         private string yearEstablished;
         private uint enrolled;
+        //enumerator declaration
         public enum SchoolType { Elementary, HighSchool, CommunityCollege, University }
 
+        //Constructor with arguments
         public School(string[] args) : base(args)
         {
             name = args[9];
             yearEstablished = args[10];
             enrolled = Convert.ToUInt32(args[11]);
+            //type = 
         }
 
+        //getter and setter for Name
         public String Name
         {
             set { name = value; }
             get { return name; }
         }
 
+        //getter for YearEstablished readonly property
         public String YearEstablished
         {
             get { return yearEstablished; }
         }
 
+        //getter and setter for Enrolled
         public uint Enrolled
         {
             set { enrolled = value; }
