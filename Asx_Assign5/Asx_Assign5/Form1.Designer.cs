39a129f471f66c2082a88e044ef7e60eee570acb
XuezhiCang <fort.cang@gmail.com>
2020-04-13 01:42:30 -0500

    knight piece

--- Asx_Assign5/Asx_Assign5/Form1.Designer.cs
+++ Asx_Assign5/Asx_Assign5/Form1.Designer.cs
@@ -32,12 +32,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1394, 848);
+            this.ClientSize = new System.Drawing.Size(1046, 678);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
 
         }
--- Asx_Assign5/Asx_Assign5/Form1.cs
+++ Asx_Assign5/Asx_Assign5/Form1.cs
@@ -19,13 +19,70 @@ namespace Asx_Assign5
             InitializeComponent();
             WindowState = FormWindowState.Maximized;
         }
-
+        int initSwitch = 0;
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            InitializeBoard(e.Graphics);
+            if (initSwitch == 0)
+            {
+                InitializeBoard(e.Graphics);
+                initSwitch = 1;
+            }
+            else
+            {
+                RedraweBoard(e.Graphics);
+            }
+
         }
 
+
+
         #region Initialize Chess Board
+
+        private void RedraweBoard(Graphics g)
+        {
+            //MessageBox.Show("inredraw");
+
+            DrawBoard(g);
+            foreach (var w in _whites)
+            {
+                g.DrawImage(w.Icon, w.X, w.Y, 50, 50);
+            }
+            foreach (var w in _blacks)
+            {
+                g.DrawImage(w.Icon, w.X, w.Y, 50, 50);
+            }
+        }
+
+
+        private void DrawBoard(Graphics g)
+        {
+
+            int _bX = 10, _bY = 10;
+            int _bWidth = 640, _bHeight = 640; //must be multiple of 8
+            Brush _borderColor = Brushes.Black;
+            Brush[] _bColor = { Brushes.DarkGray, Brushes.Beige };
+            //drawing squares
+            int spaceX = _bWidth / 8;
+            int spaceY = _bHeight / 8;
+            for (int c = 0; c < 8; c++)
+            {
+                for (int r = 0; r < 8; r++)
+                {
+                    g.FillRectangle(_bColor[(c + r) % 2], _bX + c * spaceX, _bY + r * spaceY, spaceX, spaceY);
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                g.DrawLine(new Pen(_borderColor), _bX, _bY + i * spaceY, _bX + _bWidth, _bY + i * spaceY);
+                g.DrawLine(new Pen(_borderColor), _bX + i * spaceX, _bY, _bX + i * spaceX, _bY + _bHeight);
+            }
+            //draw border
+            g.DrawRectangle(new Pen(_borderColor, 1), new Rectangle(_bX, _bY, _bWidth, _bHeight));
+
+
+        }
+
+
         private void InitializeBoard(Graphics g)
         {
             int _bX = 10, _bY = 10;
@@ -149,6 +206,10 @@ namespace Asx_Assign5
 
         }
 
+
+
+
+
         private void LoadBlackPieces()
         {
             int xSpace = -10;
@@ -226,5 +287,240 @@ namespace Asx_Assign5
         }
         #endregion
 
+
+
+
+
+        int moveSwitch = 0;
+        bool whiteTurnSwitch = true;
+        Pieces currentPiece;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //convert the X Y 
+            int boardX = (e.X - 10) / 80;
+            int boardY = (e.Y - 10) / 80;
+            Pieces temp;
+
+            //Graphics
+            Graphics g;
+            g = this.CreateGraphics();
+
+
+            //select the current piece
+            if (whiteTurnSwitch)
+            {
+                temp = _whites.Find(x => ((x.X == 30 + boardX * 80) && (x.Y == 30 + boardY * 80)));
+            }
+            else
+            {
+                temp = _blacks.Find(x => ((x.X == 30 + boardX * 80) && (x.Y == 30 + boardY * 80)));
+            }
+            
+            //if sth is selected and nothing is selected in the last step
+            if (temp != null )
+            {
+                //MessageBox.Show("step 1");
+                currentPiece = temp;
+                moveSwitch = 1;
+                return;
+            }
+
+            //sth is selected and will move in this step
+            if (moveSwitch == 1) 
+            {
+                //MessageBox.Show("step 2");
+                string currentPieceName = currentPiece.Name;
+                if (currentPieceName.Contains("Knight"))
+                {
+                    //if the knight is moved, return 0, otherwise,return 1
+                    moveSwitch = KnightProcessing(currentPiece, e.X, e.Y);
+                }
+                if (currentPieceName.Contains("Bish"))
+                {
+                    int aaaaaa = 1;
+                    BishopProcessing(currentPiece, e.X, e.Y);
+                }
+                if (moveSwitch==0)
+                {
+                    whiteTurnSwitch = !whiteTurnSwitch;
+                    RedraweBoard(g);
+                }
+        
+
+            }
+
+        }
+
+        private bool BishopProcessing(Pieces currentPiece, int X, int Y)
+        {
+            return true;
+        }
+
+        private bool checkEmptyGrid(int X, int Y)
+        {
+            //MessageBox.Show("check whether it is empty");
+            int boardX = (X - 10) / 80;
+            int boardY = (Y - 10) / 80;
+            Pieces temp1 = _whites.Find(x => ((x.X == 30 + boardX * 80) && (x.Y == 30 + boardY * 80)));
+            Pieces temp2 = _blacks.Find(x => ((x.X == 30 + boardX * 80) && (x.Y == 30 + boardY * 80)));
+
+            //no poece in that grid
+            if ((temp1 == null) || (temp2 == null))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private bool checkEnemyGrid(Pieces currentKnight, int X, int Y)
+        {
+            //MessageBox.Show("looking for enemy");
+            int currentPieceBoardX = (currentKnight.X - 10) / 80;
+            int currentPieceBoardY = (currentKnight.Y - 10) / 80;
+            int targetgridBoardX = (X - 10) / 80;
+            int targetgridBoardY = (Y - 10) / 80;
+            Pieces temp;
+
+
+            if (whiteTurnSwitch ==true)
+            {
+                temp = _blacks.Find(x => ((x.X == 30 + targetgridBoardX * 80) && (x.Y == 30 + targetgridBoardY * 80)));
+            }
+            else
+            {
+                temp = _whites.Find(x => ((x.X == 30 + targetgridBoardX * 80) && (x.Y == 30 + targetgridBoardY * 80)));
+            }
+            
+            //if enemy is not empty
+            if (temp != null)
+            {
+                return true;
+            }
+            //if enemy is empty
+            else
+            {
+                return false;
+            }
+        }
+
+        private void removeEnemyPiece(Pieces currentKnight, int X, int Y)
+        {
+            //MessageBox.Show("removing sth");
+            int currentPieceBoardX = (currentKnight.X - 10) / 80;
+            int currentPieceBoardY = (currentKnight.Y - 10) / 80;
+            int targetgridBoardX = (X - 10) / 80;
+            int targetgridBoardY = (Y - 10) / 80;
+
+            Pieces temp;
+
+
+            if (whiteTurnSwitch == true)
+            {
+                temp = _blacks.Find(x => ((x.X == 30 + targetgridBoardX * 80) && (x.Y == 30 + targetgridBoardY * 80)));
+            }
+            else
+            {
+                temp = _whites.Find(x => ((x.X == 30 + targetgridBoardX * 80) && (x.Y == 30 + targetgridBoardY * 80)));
+            }
+
+ 
+            if (whiteTurnSwitch==true)
+            {
+                _blacks.RemoveAll(x => ((x.X == 30 + targetgridBoardX * 80) && (x.Y == 30 + targetgridBoardY* 80)));
+            }
+            else
+            {
+                _whites.RemoveAll(x => ((x.X == 30 + targetgridBoardX* 80) && (x.Y == 30 + targetgridBoardY* 80)));
+            }
+
+           }
+
+        private bool CheckKnightMove(Pieces currentKnight, int X, int Y)
+        {
+
+            int currentPieceBoardX = (currentKnight.X - 10) / 80;
+            int currentPieceBoardY = (currentKnight.Y - 10) / 80;
+            int targetgridBoardX = (X - 10) / 80;
+            int targetgridBoardY = (Y - 10) / 80;
+
+            //MessageBox.Show(    Math.Abs(currentPieceBoardX - targetgridBoardX).ToString()   );
+            //MessageBox.Show(   Math.Abs(currentPieceBoardY - targetgridBoardY).ToString()    );
+
+            if (  (Math.Abs(currentPieceBoardX - targetgridBoardX) == 1) && (Math.Abs(currentPieceBoardY - targetgridBoardY) == 2)   )
+            {
+                
+                return true;
+            }
+            if ((Math.Abs(currentPieceBoardX - targetgridBoardX) == 2) && (Math.Abs(currentPieceBoardY - targetgridBoardY) == 1))
+            {
+                return true;
+            }
+            return false;
+
+
+
+
+        }
+
+
+        private int KnightProcessing(Pieces currentKnight, int X,int Y)
+        {
+            if (CheckKnightMove(currentKnight, X, Y))
+            {
+                // if the target grid is empty, move the piece on it. 
+                if (checkEmptyGrid(X, Y))
+                {
+                    currentPiece.X = 30 + ((X - 10) / 80) * 80;
+                    currentPiece.Y = 30 + ((Y - 10) / 80) * 80;
+                }
+                //if the target grid is occupied by enemy, replaceing the Enemy piece 
+                if (checkEnemyGrid(currentKnight, X, Y))
+                {
+                    //MessageBox.Show("enemy is found");
+                    removeEnemyPiece(currentKnight, X, Y);
+                    currentPiece.X = 30 + ((X - 10) / 80) * 80;
+                    currentPiece.Y = 30 + ((Y - 10) / 80) * 80;
+                }
+
+
+                if (whiteTurnSwitch == true)
+                {
+                    foreach (var w in _whites)
+                    {
+                        if (w.Name == currentPiece.Name)
+                        {
+                            w.X = currentPiece.X;
+                            w.Y = currentPiece.Y;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var w in _blacks)
+                    {
+                        if (w.Name == currentPiece.Name)
+                        {
+                            w.X = currentPiece.X;
+                            w.Y = currentPiece.Y;
+                        }
+                    }
+                }
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+
+
+
+
+
+        }
     }
 }
