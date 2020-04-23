57825c2db4dfbb671684510dd995c11475d579bc
abdul <z1836477@students.niu.edu>
2020-04-22 19:51:32 -0500

    Abdul: Charts Buttons Label

--- Asx_Assign6/Asx_Assign6/Form1.Designer.cs
+++ Asx_Assign6/Asx_Assign6/Form1.Designer.cs
@@ -36,53 +36,58 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(144, 70);
+            this.button1.Location = new System.Drawing.Point(96, 45);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 48);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Chart1";
+            this.button1.Text = "Column chart";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(144, 232);
+            this.button2.Location = new System.Drawing.Point(96, 151);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 45);
+            this.button2.Size = new System.Drawing.Size(86, 29);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Chart2";
+            this.button2.Text = "Spline Chart";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(505, 70);
+            this.button3.Location = new System.Drawing.Point(337, 45);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(129, 45);
+            this.button3.Size = new System.Drawing.Size(86, 29);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Chart3";
+            this.button3.Text = "Point Chart";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(505, 232);
+            this.button4.Location = new System.Drawing.Point(337, 151);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(129, 45);
+            this.button4.Size = new System.Drawing.Size(86, 29);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Chart4";
+            this.button4.Text = "Pie Chart";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
